448be53f9b3ff6427fbe245647e567063416620b
stirback <stirback@gmail.com>
2018-12-20 13:20:48 -0800

    Initial commit for LCD Driver, currently testing using 8 GPIO pins to test data sending with LEDs

---
+++ LCD.c
@@ -0,0 +1,35 @@
+#include "stm32f407xx.h"
+
+
+void delay(void)
+	{
+		uint32_t i=0;
+		for(i=0; i < 5000000;i++);
+}
+	
+int main(void)
+{
+// Enable the GPIO Clock for GPIO
+	RCC->AHB1ENR |= (1 << 3);
+	RCC->AHB1ENR |= (1 << 0);
+// Set any Control Registers for GPIO pins
+// Moder
+	GPIOD->MODER |= (1 << 20);
+	GPIOA->MODER |= 0b0101010101010101;
+// OTyper
+	GPIOD->OTYPER &= ~(1 << 10);
+	GPIOA->OTYPER = 0;
+// OSpeedr
+	GPIOD->OSPEEDR &= ~( (1 << 21) | (1 << 20) );
+	GPIOA->OSPEEDR = 0b1010101010101010;
+// PUPDr
+	GPIOD->PUPDR &= ~( (1 << 21) | (1 << 20) );
+	GPIOA->PUPDR = 0;
+	// Turn on the LEDs (BSRR)
+	GPIOD->BSRR = (1 << 10); //test LED
+	GPIOA->BSRR = 0b1111111111; //8 LEDs simulation data input to LCD
+while(1)
+{
+
+}
+}
---
+++ Objects/lcd.d
@@ -0,0 +1,9 @@
+.\objects\lcd.o: LCD.c
+.\objects\lcd.o: E:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.13.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f407xx.h
+.\objects\lcd.o: E:\Keil_v5\ARM\PACK\ARM\CMSIS\5.3.0\CMSIS\Include\core_cm4.h
+.\objects\lcd.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\lcd.o: E:\Keil_v5\ARM\PACK\ARM\CMSIS\5.3.0\CMSIS\Include\cmsis_version.h
+.\objects\lcd.o: E:\Keil_v5\ARM\PACK\ARM\CMSIS\5.3.0\CMSIS\Include\cmsis_compiler.h
+.\objects\lcd.o: E:\Keil_v5\ARM\PACK\ARM\CMSIS\5.3.0\CMSIS\Include\cmsis_armcc.h
+.\objects\lcd.o: E:\Keil_v5\ARM\PACK\ARM\CMSIS\5.3.0\CMSIS\Include\mpu_armv7.h
+.\objects\lcd.o: E:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.13.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
